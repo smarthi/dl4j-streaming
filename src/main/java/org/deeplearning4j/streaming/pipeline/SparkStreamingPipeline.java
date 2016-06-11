1a6813ae52a8e66f7f285616cecac64db54117cd
Adam Gibson <adam@skymind.io>
2016-06-10 17:34:39 -0700

    more cleanup

--- src/main/java/org/deeplearning4j/streaming/pipeline/SparkStreamingPipeline.java
+++ src/main/java/org/deeplearning4j/streaming/pipeline/SparkStreamingPipeline.java
@@ -1,5 +1,7 @@
 package org.deeplearning4j.streaming.pipeline;
 
+import lombok.Builder;
+import lombok.Data;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -27,6 +29,8 @@ import java.util.*;
  * Spark streaming pipeline.
  *
  */
+@Data
+@Builder
 public class SparkStreamingPipeline {
     private String kafkaTopic;
     private String inputUri;
@@ -40,9 +44,12 @@ public class SparkStreamingPipeline {
     private String sparkAppName;
     private Duration streamingDuration =  Durations.seconds(1);
     private int kafkaPartitions = 1;
+    private JavaStreamingContext jssc;
+    private SparkConf sparkConf;
+    private Function<JavaPairRDD<String, String>, Void> streamProcessor;
 
-    public void process() throws Exception {
-        if(camelContext == null)
+    public void init() throws Exception {
+        if (camelContext == null)
             camelContext = new DefaultCamelContext();
 
         camelContext.addRoutes(new CamelKafkaRouteBuilder.Builder().
@@ -51,59 +58,59 @@ public class SparkStreamingPipeline {
                         topicName(kafkaTopic).camelContext(camelContext)
                 .dataTypeUnMarshal(dataType)
                 .inputUri(inputUri).
-                        kafkaBrokerList(kafkaBroker).processor( new Processor() {
+                        kafkaBrokerList(kafkaBroker).processor(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
                         Collection<Collection<Writable>> record = (Collection<Collection<Writable>>) exchange.getIn().getBody();
                         exchange.getIn().setHeader(KafkaConstants.KEY, UUID.randomUUID().toString());
                         exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, new Random().nextInt(kafkaPartitions));
-                        byte[] bytes = new RecordSerializer().serialize(kafkaTopic,record);
+                        byte[] bytes = new RecordSerializer().serialize(kafkaTopic, record);
                         String base64 = org.apache.commons.codec.binary.Base64.encodeBase64String(bytes);
-                        exchange.getIn().setBody(base64,String.class);
+                        exchange.getIn().setBody(base64, String.class);
                     }
                 }).build());
-        camelContext.start();
-
 
 
         System.setProperty("hadoop.home.dir", hadoopHome);
-
-
-        SparkConf sparkConf = new SparkConf().setAppName(sparkAppName).setMaster(sparkMaster);
-        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, streamingDuration);
+        sparkConf = new SparkConf().setAppName(sparkAppName).setMaster(sparkMaster);
+        jssc = new JavaStreamingContext(sparkConf, streamingDuration);
         Map<String, String> kafkaParams = new HashMap<>();
-        kafkaParams.put("metadata.broker.list",kafkaBroker);
+        kafkaParams.put("metadata.broker.list", kafkaBroker);
 
-        JavaPairInputDStream<String,String> messages = KafkaUtils.createStream(
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createStream(
                 jssc,
                 zkHost,
                 "canova",
-                Collections.singletonMap(kafkaTopic,3));
-        messages.foreach(new Function<JavaPairRDD<String, String>, Void>() {
-            @Override
-            public Void call(JavaPairRDD<String, String> stringStringJavaPairRDD) throws Exception {
-                Map<String,String> map = stringStringJavaPairRDD.collectAsMap();
-                if(!map.isEmpty()) {
-                    Iterator<String> vals = map.values().iterator();
-                    while(vals.hasNext()) {
-                        try {
-                            byte[] bytes = org.apache.commons.codec.binary.Base64.decodeBase64(vals.next());
-                            Collection<Collection<Writable>> records = new RecordDeSerializer().deserialize("topic",bytes);
-                            System.out.println(records);
-                        }catch (Exception e) {
-                            System.out.println("Error serializing");
+                Collections.singletonMap(kafkaTopic, 3));
+        if(streamProcessor == null)
+            new Function<JavaPairRDD<String, String>, Void>() {
+                @Override
+                public Void call(JavaPairRDD<String, String> stringStringJavaPairRDD) throws Exception {
+                    Map<String, String> map = stringStringJavaPairRDD.collectAsMap();
+                    if (!map.isEmpty()) {
+                        Iterator<String> vals = map.values().iterator();
+                        while (vals.hasNext()) {
+                            try {
+                                byte[] bytes = org.apache.commons.codec.binary.Base64.decodeBase64(vals.next());
+                                Collection<Collection<Writable>> records = new RecordDeSerializer().deserialize("topic", bytes);
+                                System.out.println(records);
+                            } catch (Exception e) {
+                                System.out.println("Error serializing");
+                            }
                         }
+
                     }
-
+                    return null;
                 }
-                return null;
-            }
-        });
+            };
+        messages.foreach(streamProcessor);
+    }
 
+    public void process() throws Exception {
         // Start the computation
+        camelContext.start();
         jssc.start();
         jssc.awaitTermination();
-
         camelContext.stop();
 
     }
@@ -111,12 +118,4 @@ public class SparkStreamingPipeline {
 
 
 
-    public SparkStreamingPipeline(String kafkaTopic, String inputUri, String inputFormat, String kafkaBroker, String zkHost, CamelContext camelContext) {
-        this.kafkaTopic = kafkaTopic;
-        this.inputUri = inputUri;
-        this.inputFormat = inputFormat;
-        this.kafkaBroker = kafkaBroker;
-        this.zkHost = zkHost;
-        this.camelContext = camelContext;
-    }
 }
